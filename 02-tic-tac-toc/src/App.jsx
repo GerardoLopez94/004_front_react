0512ac7b3945adc7129f3f147bc4b16c47e67ec6
Gerardo94 <gerardo.a.lopez1994@gmail.com>
2024-01-28 09:01:29 -0300

    Se cambia el board para utilizar el hook useState. Se crea un componente Square

--- 02-tic-tac-toc/src/App.jsx
+++ 02-tic-tac-toc/src/App.jsx
@@ -1,30 +1,43 @@
+import { useState } from "react";
+import { Children } from "react";
 
 const TRUNS = {
     X: 'x',
     O: 'o',
 }
 
-//Creamos nuestro tablero y rellenamos con null
-const board = Array(9).fill(null);
+
+
+const Square = ({Children, updateBoard, index}) => {
+    return (
+        <div className="square">
+            {Children}
+        </div>
+    );
+}
 
 function App() {
+    //Creamos nuestro tablero y rellenamos con null
+const [board, setBoard] = useState(Array(9).fill('x'));
     return (
-        <main className= 'board'>
-            <h1>Tic Tac Toe</h1>
-            <section className='game'>
-                {
-                    board.map((pos, index) => {
-                        return (
-                            <div className="cell" key={index}>
-                                <span className="cell__content">
-                                    {index}
-                                </span>
-                            </div>
-                        )
-                    })
-                }
-            </section>
-        </main>
+      <main className= 'board'>
+        <h1>Tic Tac Toe</h1>
+        <section className='game'>
+          {
+            board.map( (square, index) => {
+                return (
+                    <Square 
+                    key={index}
+                    index={index}
+                    Children={index}
+                    >
+                       
+                    </Square>
+                )
+            })
+          }  
+        </section>
+      </main>
     )
 }
 
